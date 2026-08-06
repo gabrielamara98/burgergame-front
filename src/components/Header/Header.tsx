3815ce19c78b0c895adc67072f672b61c10a4cb5
git config --global user.name Gabriel <gabriel.amara@hotmail.com>
2026-08-06 17:55:37 -0300

    basisc style and clean files

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,20 @@
 export function Header() {
     return(
-        <header>
+        <header className="min-w-full flex justify-between border py-4 px-12 bg-red-500 items-center">
         {/* Left links */}
             <nav>
-                <ul>
-                    <li>
-                        <a>Home</a>
-                        <a>Menu</a>
-                    </li>
+                <ul className="flex gap-6">
+                    <li><a href="/" className="font-pixel">Home</a></li>
+                    <li><a href="/" className="font-pixel">Menu</a></li>
                 </ul>
             </nav>
         {/* Logo */}
-            <a>LOGO</a>
+            <a><img src=".\src\assets\logo.svg" className="w-24"></img></a>
         {/* Right Links */}
             <nav>
-                <ul>
-                    <li>
-                        <a>Reserve</a>
-                        <a>About</a>
-                    </li>
+                <ul className="flex gap-6">
+                    <li><a href="/" className="font-pixel">Reserve</a></li>
+                    <li><a href="/" className="font-pixel">About</a></li>
                 </ul>
             </nav>
         </header>
